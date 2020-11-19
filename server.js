607e1821f6268448b5b99acc8d940a762795afaf
Diego García <diegomysterio@gmail.com>
2020-11-18 21:58:08 -0600

    heroku

--- server.js
+++ server.js
@@ -53,28 +53,16 @@ app.get('/callback', (req, res) => {
         .authorizationCodeGrant(code)
         .then(data => {
             const access_token = data.body['access_token'];
-            const refresh_token = data.body['refresh_token'];
             const expires_in = data.body['expires_in'];
 
             spotifyApi.setAccessToken(access_token);
-            spotifyApi.setRefreshToken(refresh_token);
 
             console.log('access_token:', access_token);
-            console.log('refresh_token:', refresh_token);
 
             console.log(
                 `Sucessfully retreived access token. Expires in ${expires_in} s.`
             );
             res.send('Success! You can now close the window.');
-
-            setInterval(async () => {
-                const data = await spotifyApi.refreshAccessToken();
-                const access_token = data.body['access_token'];
-
-                console.log('The access token has been refreshed!');
-                console.log('access_token:', access_token);
-                spotifyApi.setAccessToken(access_token);
-            }, expires_in / 2 * 1000);
         })
         .catch(error => {
             console.error('Error getting Tokens:', error);
